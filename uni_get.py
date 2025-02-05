34da8401ceca039efacf26222a248dd748f2aca0
asaburodesu <136838104+asaburodesu@users.noreply.github.com>
2025-02-05 23:19:12 +0900

    <Fix>tweet text

--- uni_get.py
+++ uni_get.py
@@ -31,7 +31,7 @@ for i in range(47):
         location = str(tempo.find(class_="store_map")).replace('\n', '')
         detail = re.search('sid=\d+',str(tempo.find(class_="store_bt")).replace('\n', ''))[0]
         detailurl = "https://location.am-all.net/alm/shop?gm=109&astep=0&" + detail
-        twitter = "https://twitter.com/intent/tweet?text=" + address + "%2C" + name
+        twitter = "https://twitter.com/intent/tweet?text=" + address + name
       
         pattern = '@.*&'
         result = re.findall(pattern, location)[0].replace('@','').replace('&','')
